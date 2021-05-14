31cd684eabf3672fc6c5e83d23106c906559e8dc
iamjackydev <iamjackydev@gmail.com>
2021-05-13 20:17:22 -0400

    fix error in file

--- server.js
+++ server.js
@@ -23,24 +23,24 @@ app.get("/users/:id", function (req, res) {
 });
 
 app.post("/login", function (req, res) {
-    const username = req.body.username;
-    const password = req.body.password;
+     const username = req.body.username;
+     const password = req.body.password;
 
-    const mockUsername = "billyTheKid";
-    const mockPassword = "superSecret";
+     const mockUsername = "billyTheKid";
+     const mockPassword = "superSecret";
 
-    if (username === mockUsername && password === mockPassword) {
-        res.json({
-            success = true,
-            message: "password and username match!",
-            token: "encrypted token goes here"
-        });
-    } else {
-        res.json({
-            success: false,
-            message: "password and username do not match"
-        });
-    }
+     if (username === mockUsername && password === mockPassword) {
+          res.json({
+               success: true,
+               message: "password and username match!",
+               token: "encrypted token goes here",
+          });
+     } else {
+          res.json({
+               success: false,
+               message: "password and username do not match",
+          });
+     }
 });
 
 app.listen(8000, function () {
